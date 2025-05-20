63463fcc2c8a862378461c375c2789b77cfee216
ZaidMousa10 <zaidmousa0301@gmail.com>
2025-05-20 17:53:57 +0300

    Update MainActivity

--- app/src/main/java/com/example/schoolapp/MainActivity.java
+++ app/src/main/java/com/example/schoolapp/MainActivity.java
@@ -2,21 +2,29 @@ package com.example.schoolapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.schoolapp.AddTeacherActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-        // Immediately go to AddTeacherActivity
         Intent intent = new Intent(MainActivity.this, AddTeacherActivity.class);
         startActivity(intent);
-
-        // Optional: finish MainActivity so user can't go back to it
-        finish();
     }
 }
