64493d71309e99f6ac402a6c682dc042668115df
omarkashour <1210082@student.birzeit.edu>
2025-05-26 16:18:33 +0300

    Started implementing TeacherSendMessage2

--- app/src/main/java/com/example/schoolapp/TeacherSendMessage2.java
+++ app/src/main/java/com/example/schoolapp/TeacherSendMessage2.java
@@ -1,24 +1,60 @@
 package com.example.schoolapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.schoolapp.models.Student;
+import com.google.gson.Gson;
 
 public class TeacherSendMessage2 extends AppCompatActivity {
+
+    private TextView tvTitle, tvToStudent, tvToStudentId;
+    private EditText etTitle, etContent;
+    private Button btnCancel, btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_teacher_send_message2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setupViews();
+        setupIntent();
+    }
+
+    private void setupIntent() {
+        Intent intent = getIntent();
+        Gson gson = new Gson();
+        String json =  intent.getStringExtra("student");
+        Student student = gson.fromJson(json , Student.class);
+        tvToStudent.setText("To Student: " + student.getFirstName() + " " + student.getLastName());
+        tvToStudentId.setText("Student ID: " + student.getUser_id());
+
+    }
+
+    private void setupViews() {
+        tvTitle = findViewById(R.id.tvTitle);
+        tvToStudent = findViewById(R.id.tvToStudent);
+        tvToStudentId = findViewById(R.id.tvToStudentId);
+
+        etTitle = findViewById(R.id.etTitle);
+        etContent = findViewById(R.id.etContent);
+
+        btnCancel = findViewById(R.id.btnCancel);
+        btnSend = findViewById(R.id.btnSend);
+
+        btnCancel.setOnClickListener(v -> {
+            finish();
+        });
+
+        btnSend.setOnClickListener(v -> {
+
         });
     }
 }
