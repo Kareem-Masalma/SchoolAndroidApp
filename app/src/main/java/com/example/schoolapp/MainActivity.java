15a3439574ceee89cbe8af04b0a37a63dfe65798
ZaidMousa10 <zaidmousa0301@gmail.com>
2025-06-09 14:43:57 +0300

    Update the styling and correct TeacherDA

--- app/src/main/java/com/example/schoolapp/MainActivity.java
+++ app/src/main/java/com/example/schoolapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.schoolapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +10,6 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.example.schoolapp.json_helpers.LocalDateAdapter;
-import com.example.schoolapp.models.Role;
-import com.example.schoolapp.models.SchoolClass;
-import com.example.schoolapp.models.Teacher;
-import com.example.schoolapp.models.User;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.time.LocalDate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,37 +38,14 @@ public class MainActivity extends AppCompatActivity {
 
 //        Intent intent = new Intent(MainActivity.this, TeacherSendMessage1.class);
 
-//        SharedPreferences prefs = getSharedPreferences(Login.LOGGED_IN_USER, MODE_PRIVATE);
+//        SharedPreferences prefs = getSharedPreferences("UserSession", MODE_PRIVATE);
 //        SharedPreferences.Editor editor = prefs.edit();
 //        editor.putInt("user_id", 3);
 //        editor.apply();
-
-//        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-//        boolean isLoggedIn = prefs.getBoolean(Login.LOGGED_IN, false);
 //
-//        if (isLoggedIn) {
-//            String json = prefs.getString(Login.LOGGED_IN_USER, null);
-//            if (json != null) {
-//                Gson gson = new GsonBuilder()
-//                        .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
-//                        .create();
-//                User user = gson.fromJson(json, User.class);
 //
-//                // Access the user_id
-//                int userId = user.getUser_id();
-//
-//                // If needed, pass user_id to next activity via intent
-//                Intent intent = new Intent(MainActivity.this, AssignmentListActivity.class);
-//                intent.putExtra("USER_ID", userId);
-//                startActivity(intent);
-//                finish();
-//                return;
-//            }
-//        }
-//
-
 //        Intent intent = new Intent(MainActivity.this,AssignmentListActivity.class);
-//        SchoolClass sClass = new SchoolClass(1, "10-A", 1, "John",11);
+//        Class sClass = new Class(1, "10-A", 1, "John",11);
 //        Teacher teacher = new Teacher(1,"Joan","Smith", LocalDate.parse("1980-03-15"), "123 Elm St","555-1001", Role.TEACHER,"Mathematics");
 //        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
 //        String json = gson.toJson(teacher);
@@ -87,9 +53,8 @@ public class MainActivity extends AppCompatActivity {
 //        String json1 = gson.toJson(sClass);
 //        intent.putExtra(AddSchedule.CLASS, json1);
 
-//        Intent intent = new Intent(MainActivity.this, AddTeacherActivity.class);
-
         Intent intent = new Intent(MainActivity.this, Login.class);
+
         startActivity(intent);
     }
 }
