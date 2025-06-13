43589b948b31dffa933a5b95eeee86c09dbbb934
Omar Kashour <109619699+omarkashour@users.noreply.github.com>
2025-06-13 12:45:00 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/schoolapp/MainActivity.java
+++ app/src/main/java/com/example/schoolapp/MainActivity.java
@@ -21,64 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-//        Intent intent = new Intent(MainActivity.this, AddSchedule.class);
-//        Intent intent = new Intent(MainActivity.this, AddStudent.class);
-
-
-        // take attendance activity
-//        Intent intent = new Intent(MainActivity.this, TakeAttendance.class);
-//        SchoolClass schoolClass = new SchoolClass(1,"6-B",2,1);
-//        Gson gson = new Gson();
-//        String json = gson.toJson(schoolClass);
-//        intent.putExtra("schoolClass", json);
-
-//        Intent intent = new Intent(MainActivity.this, TeacherSendMessage1.class);
-
-//        SharedPreferences prefs = getSharedPreferences("UserSession", MODE_PRIVATE);
-//        SharedPreferences.Editor editor = prefs.edit();
-//        editor.putInt("user_id", 3);
-//        editor.apply();
-//
-//
-//        Intent intent = new Intent(MainActivity.this,AssignmentListActivity.class);
-//        Class sClass = new Class(1, "10-A", 1, "John",11);
-//        Teacher teacher = new Teacher(1,"Joan","Smith", LocalDate.parse("1980-03-15"), "123 Elm St","555-1001", Role.TEACHER,"Mathematics");
-//        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
-//        String json = gson.toJson(teacher);
-//        intent.putExtra(AddSchedule.TEACHER, json);
-//        String json1 = gson.toJson(sClass);
-//        intent.putExtra(AddSchedule.CLASS, json1);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(
-                    "assignment_reminder_channel",
-                    "Assignment Reminders",
-                    NotificationManager.IMPORTANCE_HIGH
-            );
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            if (manager != null) {
-                manager.createNotificationChannel(channel);
-            }
-        }
-        NotificationChannel channel = new NotificationChannel(
-                "assignment_reminder_channel",
-                "Assignment Reminders",
-                NotificationManager.IMPORTANCE_HIGH
-        );
-        channel.enableLights(true);
-        channel.enableVibration(true);
-        NotificationManager manager = getSystemService(NotificationManager.class);
-        manager.createNotificationChannel(channel);
-
-        new SendAssignmentActivity().scheduleReminder("Test Assignment", "", 0, 0);
-
-
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         Intent intent = new Intent(MainActivity.this, Login.class);
 
